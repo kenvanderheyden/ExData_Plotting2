6337a0301940e0f5a7d0a65c995a86ba7254b49f
Ken Vanderheyden <ken.vanderheyden@archimiddle.com>
2015-09-23 15:22:08 +0200

    plot 4 added

--- plot4.R
+++ plot4.R
@@ -9,24 +9,29 @@ setwd("~/Documents/Machine Learning/exploratory data analysis/ExData_Plotting2")
 
 # read in the data (code from the assignment, with altered location to match local data files)
 NEI <- readRDS("data/summarySCC_PM25.rds")
-SCC <- readRDS("data/Source_Classification_Code.rds") # strictly this line of import is not needed for this plot, but it is a small data set compare to the NEI set. 
+SCC <- readRDS("data/Source_Classification_Code.rds") 
 
-NEI <- filter(NEI, fips == "24510")
+# filter to keep only coal combustion-related sources
+SCC <- SCC %>% filter(grepl(".+Coal$", SCC.Level.Three))
+SCC <- SCC %>% filter(grepl("^.+Combustion.+", SCC.Level.One))
+
+# inner join the NEI datset with the SCC filtered set, to keep only the records with the coal combustion related data in the NEI set
+NEI <- inner_join(NEI, SCC, by = "SCC")
 
 # create data frame, with dplyr package, calc sum of emissions per year
-emisByTypePerYear <- NEI %>% group_by(year, type) %>% summarise(sum(Emissions)) #%>% group_by(type)
+emisCoalCombPerYear <- NEI %>% group_by(year) %>% summarise(sum(Emissions))
 
 # rename the column names for easy use in plot 
-colnames(emisByTypePerYear) <- c("year", "type", "emis")
+colnames(emisCoalCombPerYear) <- c("year", "emis")
 
 # check the created emissions data in text format for verification
-print(emisByTypePerYear)
+print(emisCoalCombPerYear)
 
 # generate the plot
-plot4 <- ggplot(emisByTypePerYear, aes(x=year, y=emis, group=type, color=type)) + 
+plot4 <- ggplot(emisCoalCombPerYear, aes(x=year, y=emis)) + 
     geom_line() + 
     geom_point( size=4, shape=21, fill="white") +
-    ggtitle("Plot 4 - USA: total PM25 emissions by type per year") +
+    ggtitle("Plot 4 - USA: total PM25 emissions from coal combustion related sources") +
     ylab("emission")
 
 # draw the plot 
